e25ceda5ed8294b93fbdb949fe11d66de39b2830
qjawnsdl <165367880+qjawnsdl@users.noreply.github.com>
2024-04-04 18:25:58 +0900

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,40 +1,28 @@
-import java.util.Random;
+import java.sql.SQLOutput;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        int[] intArray = new int[50];
+    int radious;
 
-        Random random = new Random();
-
-
-        for (int i = 0; i < intArray.length; i++) {
-            intArray[i] = random.nextInt(10);
-        }
-
-        int a = 0;
-        int b = 0;
-        int c = 0;
-
-
-        for (int i = 0; i < 50; i++) {
-            System.out.print(intArray[i]);
-            if ((i+1)%5 == 0)
-                System.out.println("/");//교수님 이거 이해가 안됩니다 왜 줄 바꿈이 이루어 지는거죠?
-
-            if (intArray[i] % 2 == 0) {
-                a++;
-            }
-            if (intArray[i] % 3 == 0) {
-                b++;
-            }
-            if (intArray[i] % 5 == 0) {
-                c++;
-            }
-        }
-
-
-        System.out.println("2의 배수 개수: " + a);
-        System.out.println("3의 배수 개수: " + b);
-        System.out.println("5의 배수 개수: " + c);
+    public Main () {}
+    public double getArea() {
+        return 3.14*radious*radious;
     }
+    public double getCircle() {
+        return 2*3.14*radious;
+    }
+
+
+    public static void main(String[] args) {
+        int b =2;
+
+        Main a;
+        a = new Main();
+        Scanner scanner = new Scanner(System.in);
+        a.radious= scanner.nextInt();
+        System.out.println("원의 넓이 :" + a.getArea());
+        System.out.println("원의 둘레 :" + a.getCircle());
+
+
+        }
 }
