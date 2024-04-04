852421572e9058a58d28037f2687c0775ebefe0d
qjawnsdl <165367880+qjawnsdl@users.noreply.github.com>
2024-04-04 18:22:26 +0900

    Add files via upload

---
+++ piramid.java
@@ -0,0 +1,11 @@
+public class piramid {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            for (int a = 0; a < i + 1; a++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
+}
